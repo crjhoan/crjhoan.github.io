2e8c887bb7e1b83079644df2b0fd3329df51325b
crjhoan <crjhoan@gmail.com>
2018-04-26 10:56:01 -0500

    mysql

--- tecweb/ejemplos-msql/kardex/conexion.php
+++ tecweb/ejemplos-msql/kardex/conexion.php
@@ -1,4 +1,15 @@
 <?php
-$fuente="mysql:host=localhost;dbname=calificaciones";
+$fuente="mysql:host=localhost;dbname=kardexdb";
+$usuario="root";
+$contraseña="root";
+try{
 $conexion=new PDO($fuente,$usuario,$contraseña);
+echo 'Conexion establecida...';
+$sql ="SELECT * FROM 'calificaciones'";
+$resultado=$conexion->query($sql);
+var_dump($resultado);
+}
+catch(PDOException $ex){
+    echo 'Error en la conexion'.$ex->getMessage();
+}
 ?>
