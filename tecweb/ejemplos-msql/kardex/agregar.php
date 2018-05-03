a7eed8c6fc2e79e6ec0c53e6461c89ea37c4c885
crjhoan <crjhoan@gmail.com>
2018-05-03 10:53:46 -0500

    agregar en otra pagina

---
+++ tecweb/ejemplos-msql/kardex/agregar.php
@@ -0,0 +1,34 @@
+<?php
+include 'conexion.php';
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $materia=$_POST['materia'];
+    $calificacion=$_POST['calificacion'];
+    $sql="INSERT INTO `calificaciones` (`id`, `materia`, `calificacion`) VALUES (NULL, '$materia', '$calificacion')";
+    $resultado=$conexion->query($sql);
+    //var_dump($resultado);
+    if($resultado==false){
+        die("Error al momento de insertar en la base de datos");
+    }
+    header("Location: index.php");
+}
+?>
+
+<?php include 'encabezaado.php'?>
+        <div class="row">
+        <div class="col-md-8">
+      <form action="" method='POST'>
+            <div class="form-group">
+                <label for="">Materia</label>
+                <input type="text"class="form-control" name="materia" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="form-group">
+              <label for="">Calificación</label>
+              <input type="number"
+                class="form-control" name="calificacion" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <button type="submit" class="btn btn-primary">Agregar</button>
+      </form>
+      </div>
+        </div>
+        <?php include 'pie.php';
--- tecweb/ejemplos-msql/kardex/index.php
+++ tecweb/ejemplos-msql/kardex/index.php
@@ -19,28 +19,17 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $calificacion=$_POST['calificacion'];
     $sql="INSERT INTO `calificaciones` (`id`, `materia`, `calificacion`) VALUES (NULL, '$materia', '$calificacion')";
     $resultado=$conexion->query($sql);
-    var_dump($resultado);
+    //var_dump($resultado);
     if($resultado==false){
         die("Error al momento de insertar en la base de datos");
     }
+    header("Location: index.php");
 }
 
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  </head>
-  <body>
-  <div class="container">
+<?php include 'encabezaado.php'?>
     <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-10">
       <table class="table">
           <thead>
               <tr>
@@ -59,27 +48,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
               <?php endforeach ?>
           </tbody>
       </table>
-      </div>
-      <div class="col-md-4">
-      <form action="" method='POST'>
-            <div class="form-group">
-                <label for="">Materia</label>
-                <input type="text"class="form-control" name="materia" id="" aria-describedby="helpId" placeholder="">
-            </div>
-            <div class="form-group">
-              <label for="">Calificación</label>
-              <input type="number"
-                class="form-control" name="calificacion" id="" aria-describedby="helpId" placeholder="">
-            </div>
-            <button type="submit" class="btn btn-primary">Agregar</button>
-      </form>
+        <a href="agregar.php" class="btn btn-primary">Agregar nueva materia</a>
       </div>
       </div>
-      </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+ <?php include 'pie.php';
